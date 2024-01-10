e4306e3c1450b0ddf8e69ca27ffd426247d2e495
Romario Bishop <romariobishop05@gmail.com>
2024-01-09 21:42:48 -0400

    Added function that returns the computer's choice

---
+++ script.js
@@ -0,0 +1,18 @@
+function getComputerChoice(){
+    let randomNum = Math.floor((Math.random()*3) + 1);
+    console.log(randomNum, typeof randomNum);
+
+    switch (randomNum){
+        case 1:
+            return "Rock";
+        case 2:
+            return "Paper";
+        case 3:
+            return "Scissors";
+        default:
+            return "An error accoured";            
+    }
+    
+}
+let test= getComputerChoice();
+alert(test);
